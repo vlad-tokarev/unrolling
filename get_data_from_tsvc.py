e09c39cc2871ba335b613a0f6a9ed0e77549341d
maxberezov <maxberezov@gmail.com>
2019-11-15 15:04:11 +0100

    main func

--- get_data_from_tsvc.py
+++ get_data_from_tsvc.py
@@ -8,8 +8,15 @@ from TSVC.run_exe import execute_all
 from settings import number_of_times
 from TSVC.delete import delete_files
 
-delete_files(tsvc_benchmark_path, ".tpips")
-generate_files(max_unrolling, tsvc_benchmark_path)
-delete_files(tsvc_benchmark_path, ".out")
-unroll_all(tsvc_benchmark_path, tsvc_measurement_path, optimization)
-execute_all(tsvc_measurement_path, number_of_times)
+
+def main():
+    delete_files(tsvc_benchmark_path, ".tpips")
+    generate_files(max_unrolling, tsvc_benchmark_path)
+    delete_files(tsvc_benchmark_path, ".out")
+    unroll_all(tsvc_benchmark_path, tsvc_measurement_path, optimization)
+    execute_all(tsvc_measurement_path, number_of_times)
+
+if __name__ == '__main__':
+    main()
+
+
