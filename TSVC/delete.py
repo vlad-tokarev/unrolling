49a16060f023c9197f141cc83774dcf5706a5d26
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2019-11-16 16:43:43 +0300

    remove

--- TSVC/delete.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def delete_files(path, extension):
-    files = os.listdir(path)
-    for item in files:
-        if item.endswith(extension):
-            os.remove(os.path.join(path, item))
